275e09037d44373545180c552cbe0da4e9960b04
Manilajoy14 <mmarkhonreck@gmail.com>
2026-07-12 12:08:32 +0000

    remediationagain

--- OneDrive/Documents/Testing Project/main.tf
+++ OneDrive/Documents/Testing Project/main.tf
@@ -2,5 +2,6 @@ resource "aws_iam_account_password_policy" "strickcompliance" {
       password_reuse_prevention = var.password_reuse_prevention
       max_password_age = var.max_password_age
       require_lowercase_characters = var.require_lowercase_characters
+      minimum_password_length = var.mininum_password_length
   
 }
