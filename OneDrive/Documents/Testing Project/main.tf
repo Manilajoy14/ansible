2e6fbc6f5495dffec5f83fedcc161297ad3112a7
Manilajoy14 <mmarkhonreck@gmail.com>
2026-09-14 15:24:39 +0000

    addinginbound

--- OneDrive/Documents/Testing Project/main.tf
+++ OneDrive/Documents/Testing Project/main.tf
@@ -10,7 +10,7 @@ resource "aws_security_group_rule" "inbound"{
       protocol = "tcp"
       cidr_blocks = [
       "192.168.1.2/32", 
-      "192.168.1.2/32"
+      "192.168.1.43/32"
       ]
       security_group_id = aws_security_group.test.id
 }
