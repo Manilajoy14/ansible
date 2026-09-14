1eb3458069e3688509a210685ac6fb7b9472020f
Manilajoy14 <mmarkhonreck@gmail.com>
2026-09-14 16:36:04 +0000

    addinginbound

--- OneDrive/Documents/Testing Project/main.tf
+++ OneDrive/Documents/Testing Project/main.tf
@@ -11,7 +11,7 @@ resource "aws_security_group_rule" "allow"{
       to_port = 443
       protocol = "tcp"
       cidr_blocks = ["192.168.1.1/32"]
-      security_group_id = aws_security_group.test.id
+      security_group_id = data.aws_security_group.test.id
 }
 
 resource "aws_security_group_rule" "allowssh"{
