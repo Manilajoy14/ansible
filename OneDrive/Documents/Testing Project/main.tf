a1613739fe88d086223935f7749689452bf6d3fd
Manilajoy14 <mmarkhonreck@gmail.com>
2026-09-14 15:19:00 +0000

    allowingress

--- OneDrive/Documents/Testing Project/main.tf
+++ OneDrive/Documents/Testing Project/main.tf
@@ -8,6 +8,6 @@ resource "aws_security_group_rule" "inbound"{
       from_port = 443
       to_port = 443
       protocol = "tcp"
-      cidr_blocks = [192.168.1.2/32]
+      cidr_blocks = ["192.168.1.2/32"]
       security_group_id = aws_security_group.test.id
 }
