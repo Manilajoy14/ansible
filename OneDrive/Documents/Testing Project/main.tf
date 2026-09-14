a6985cfe216e68b429e8e85b157f1852f0ad35f2
Manilajoy14 <mmarkhonreck@gmail.com>
2026-09-14 16:07:59 +0000

    addinginbound

--- OneDrive/Documents/Testing Project/main.tf
+++ OneDrive/Documents/Testing Project/main.tf
@@ -6,15 +6,13 @@ data "aws_security_group" "test"{
 }
 
 resource "aws_security_group_rule" "allowinbound"{
-      lifecycle {
-            prevent_destroy = true
-      }
+      for_each = var.allowinbound
 
       type = "ingress"
       from_port = 443
       to_port = 443
       protocol = "tcp"
-      cidr_blocks = var.allowinbound
+      cidr_blocks = [each.value]
       security_group_id = data.aws_security_group.test.id
 }
 
