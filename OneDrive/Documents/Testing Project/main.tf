20810fe5397d87e85fb62db3de8ca4e6a139541f
Manilajoy14 <mmarkhonreck@gmail.com>
2026-09-14 15:04:22 +0000

    addingip

--- OneDrive/Documents/Testing Project/main.tf
+++ OneDrive/Documents/Testing Project/main.tf
@@ -7,6 +7,8 @@ resource "aws_security_group" "test" {
             to_port = 22
             protocol = "tcp"
             cidr_blocks = ["0.0.0.0/0"]
+      }
+      ingress {
             from_port = 443
             to_port = 443
             cidr_blocks =["192.168.1.21/32"]
